8dcd6b68429c9839c28811cc4b344c4cebb45af9
SergioGit66 <sergiodomingueztenorio66@gmail.com>
2023-06-29 08:20:46 +0200

    done

---
+++ src/components/Tweet.js
@@ -0,0 +1,38 @@
+function Tweet(props) {
+    return (
+      <div className="tweet">
+        <img
+          src={props.image}
+          className="profile"
+          alt="profile"
+        />
+  
+        <div className="body">
+          <div className="top">
+            <span className="user">
+              <span className="name">{props.name}</span>
+              <span className="handle">{props.handle}</span>
+            </span>
+  
+            <span className="timestamp">{props.timestamp}</span>
+          </div>
+  
+          <p className="message">
+            {props.message}
+          </p>
+  
+          <div className="actions">
+            {/* Font Awesome icons */}
+            <i className="far fa-comment"></i>
+            <i className="fas fa-retweet"></i>
+            <i className="far fa-heart"></i>
+            <i className="fas fa-share"></i>
+          </div>
+        </div>
+  
+        <i className="fas fa-ellipsis-h"></i>
+      </div>
+    );
+  }
+  
+  export default Tweet;
